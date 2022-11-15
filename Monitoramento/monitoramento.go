e5074d8caeb7d41928803b46b3bd1395c6862a84
Gabriel <gabriel@MacBook.local>
2022-11-14 21:14:03 -0800

    Inclusao da funcao LerSites

--- Monitoramento/monitoramento.go
+++ Monitoramento/monitoramento.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	Exibicao()
-	escolhaOpcao()
 
+	for {
+		escolhaOpcao()
+	}
 }
 
 func escolhaOpcao() {
@@ -40,4 +46,48 @@ func Exibicao() {
 	fmt.Println("Sejam bem-vindo ao programa de Monitoramento")
 	fmt.Println("Programa desenvolvido por", name, "- Versão 2.0")
 
+}
+
+func monitoramentoSites() {
+
+}
+
+func lerSites() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Apresentou erro na função lerSites", err)
+	}
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+	return sites
+}
+
+func testaSites(site string) {
+
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Erro na função testa Sites", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "apresentou erro no carregamento")
+	}
 }
